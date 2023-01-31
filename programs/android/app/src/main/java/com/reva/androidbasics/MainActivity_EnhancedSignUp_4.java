964f27ad7faae12ee086f25d453ba599b91cff68
raghuprasadks <prasadraghuks@gmail.com>
2023-01-31 05:52:39 +0530

    updated

--- programs/android/app/src/main/java/com/reva/androidbasics/MainActivity_EnhancedSignUp_4.java
+++ programs/android/app/src/main/java/com/reva/androidbasics/MainActivity_EnhancedSignUp_4.java
@@ -2,7 +2,12 @@ package com.reva.androidbasics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -28,5 +33,30 @@ public class MainActivity_EnhancedSignUp_4 extends AppCompatActivity {
                 Toast.makeText(MainActivity_EnhancedSignUp_4.this, "Selected Radio Button is : " + radioButton.getText(), Toast.LENGTH_SHORT).show();
             }
         });
+
+        CheckBox chkUG = findViewById(R.id.checkBoxUG);
+        chkUG.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    Toast.makeText(MainActivity_EnhancedSignUp_4.this, "Checked : " + chkUG.getText(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        // get reference to the string array that we just created
+       // val languages = resources.getStringArray(R.array.programming_languages)
+        String[] languages =getResources().getStringArray(R.array.programming_languages);
+        // create an array adapter and pass the required parameter
+        // in our case pass the context, drop down layout , and array.
+        //val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_menu, languages)
+        ArrayAdapter arrayAdapter= new ArrayAdapter(MainActivity_EnhancedSignUp_4.this,R.layout.dropdown_item,languages);
+        // get reference to the autocomplete text view
+        AutoCompleteTextView autoCompleteTextView= findViewById(R.id.autoCompleteTextView);
+        //  val autocompleteTV = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+                // set adapter to the autocomplete tv to the arrayAdapter
+              //  autocompleteTV.setAdapter(arrayAdapter)
+        autoCompleteTextView.setAdapter(arrayAdapter);
     }
 }
