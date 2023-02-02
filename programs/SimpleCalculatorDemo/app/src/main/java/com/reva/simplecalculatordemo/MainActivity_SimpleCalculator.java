35afe9ec6f471cc4aa548a56de154467896b66e7
raghuprasadks <prasadraghuks@gmail.com>
2023-02-02 13:10:52 +0530

    updated

---
+++ programs/SimpleCalculatorDemo/app/src/main/java/com/reva/simplecalculatordemo/MainActivity_SimpleCalculator.java
@@ -0,0 +1,14 @@
+package com.reva.simplecalculatordemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_SimpleCalculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_simple_calculator);
+    }
+}
